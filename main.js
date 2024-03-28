6c5a3ded3b2feb2ddca3df90d65522a609251849
Nico <nicoalexandrekurniawan@gmail.com>
2024-03-28 22:03:51 +0700

    First commit

---
+++ main.js
@@ -0,0 +1,171 @@
+function main() {
+    var CANVAS = document.getElementById("your_canvas");
+    CANVAS.width = window.innerWidth;
+    CANVAS.height = window.innerHeight;
+
+    try {
+        GL = CANVAS.getContext("webgl", { antialias: true });
+    } catch (e) {
+        alert("WebGL context cannot be initialized");
+        return false;
+    }
+
+    // CAMERA
+    var AMORTIZATION = 0.95;
+    var dX = 0, dY = 0;
+    var drag = false;
+    var THETA = 0, PHI = 0;
+
+    var x_prev, y_prev;
+
+    var mouseDown = function (e) {
+        drag = true;
+        x_prev = e.pageX, y_prev = e.pageY;
+        e.preventDefault();
+        return false;
+    };
+
+    var mouseUp = function (e) {
+        drag = false;
+    };
+
+    var mouseMove = function (e) {
+        if (!drag) return false;
+        dX = (e.pageX - x_prev) * 2 * Math.PI / CANVAS.width,
+            dY = (e.pageY - y_prev) * 2 * Math.PI / CANVAS.height;
+        THETA += dX;
+        PHI += dY;
+        x_prev = e.pageX, y_prev = e.pageY;
+        e.preventDefault();
+    };
+
+    CANVAS.addEventListener("mousedown", mouseDown, false);
+    CANVAS.addEventListener("mouseup", mouseUp, false);
+    CANVAS.addEventListener("mouseout", mouseUp, false);
+    CANVAS.addEventListener("mousemove", mouseMove, false);
+
+    // MATRIX
+    var PROJMATRIX = glMatrix.mat4.create();
+    glMatrix.mat4.perspective(PROJMATRIX, 17, CANVAS.width / CANVAS.height, 1, 100);
+    var VIEWMATRIX = glMatrix.mat4.create();
+    glMatrix.mat4.translate(VIEWMATRIX, VIEWMATRIX, [0, 0, -5]);
+
+    // OBJECT
+    var object_vertex = [
+        -1, -1, -1, 1, 1, 0,
+        1, -1, -1, 1, 1, 0,
+        1, 1, -1, 1, 1, 0,
+        -1, 1, -1, 1, 1, 0,
+        -1, -1, 1, 0, 0, 1,
+        1, -1, 1, 0, 0, 1,
+        1, 1, 1, 0, 0, 1,
+        -1, 1, 1, 0, 0, 1,
+        -1, -1, -1, 0, 1, 1,
+        -1, 1, -1, 0, 1, 1,
+        -1, 1, 1, 0, 1, 1,
+        -1, -1, 1, 0, 1, 1,
+        1, -1, -1, 1, 0, 0,
+        1, 1, -1, 1, 0, 0,
+        1, 1, 1, 1, 0, 0,
+        1, -1, 1, 1, 0, 0,
+        -1, -1, -1, 1, 0, 1,
+        -1, -1, 1, 1, 0, 1,
+        1, -1, 1, 1, 0, 1,
+        1, -1, -1, 1, 0, 1,
+        -1, 1, -1, 0, 1, 0,
+        -1, 1, 1, 0, 1, 0,
+        1, 1, 1, 0, 1, 0,
+        1, 1, -1, 0, 1, 0
+    ];
+    var object_faces = [
+        0, 1, 2,
+        0, 2, 3,
+        4, 5, 6,
+        4, 6, 7,
+        8, 9, 10,
+        8, 10, 11,
+        12, 13, 14,
+        12, 14, 15,
+        16, 17, 18,
+        16, 18, 19,
+        20, 21, 22,
+        20, 22, 23];
+
+    // Shader
+    var shader_vertex_source = `
+      attribute vec3 position;
+      attribute vec3 color;
+      uniform mat4 Pmatrix;
+      uniform mat4 Vmatrix;
+      uniform mat4 Mmatrix;
+      varying vec3 vColor;
+      void main(void) {
+      gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.0);
+      vColor = color;
+      }`;
+    var shader_fragment_source = `
+      precision mediump float;
+      varying vec3 vColor;
+      void main(void) {
+      gl_FragColor = vec4(vColor, 1.);
+      }`;
+
+    // Object creating
+    var cube = new Object([...object_vertex], [...object_faces], shader_vertex_source, shader_fragment_source);
+    var child_cube = new Object([...object_vertex], [...object_faces], shader_vertex_source, shader_fragment_source);
+    var grandchild_cube = new Object([...object_vertex], [...object_faces], shader_vertex_source, shader_fragment_source);
+
+    // Designing the object
+    child_cube.setScale(0.5, 1.5, 0.5);
+    child_cube.setRotation(0, 0, 0.5);
+    child_cube.setPosition(0.7, -2, 0);
+    grandchild_cube.setScale(1, 0.1, 0.2);
+    grandchild_cube.setRotation(0, 0, 0.5);
+    grandchild_cube.setPosition(1.3, -3, 0.7);
+
+    // Linking the objects and add point of origin
+    cube.addChild(child_cube, 0, -0.5, 0);
+    child_cube.addChild(grandchild_cube, 1.3, -3, 0.7);
+
+    // Transform the combined object
+    // cube.scale(0.5, 0.5, 0.5);
+    // cube.rotate(0.6, 0.5, 0.9);
+    // cube.translate(0, -1, 0);
+
+    // Drawing
+    GL.clearColor(0.0, 0.0, 0.0, 0.0);
+    GL.enable(GL.DEPTH_TEST);
+    GL.depthFunc(GL.LEQUAL);
+    GL.clearDepth(1.0);
+
+    var time_prev = 0;
+    var animate = function (time) {
+        var dt = (time - time_prev);
+        if (!drag) {
+            dX *= AMORTIZATION, dY *= AMORTIZATION;
+            THETA += dX, PHI += dY;
+        }
+
+        VIEWMATRIX = glMatrix.mat4.create();
+        glMatrix.mat4.translate(VIEWMATRIX, VIEWMATRIX, [0, 0, -5]);
+        glMatrix.mat4.rotateX(VIEWMATRIX, VIEWMATRIX, PHI);
+        glMatrix.mat4.rotateY(VIEWMATRIX, VIEWMATRIX, THETA);
+
+        GL.viewport(0, 0, CANVAS.width, CANVAS.height);
+        GL.clear(GL.COLOR_BUFFER_BIT | GL.D_BUFFER_BIT);
+
+        // Moving & Rotating the objects 
+        cube.rotate(0.01, 0, 0);
+        child_cube.rotate(0, 0.02, 0);
+        grandchild_cube.rotate(0, 0, 0.5);
+        cube.translate(0, 0.1 * Math.sin(time * Math.PI / 360.0), 0);
+
+        cube.setUniform4(PROJMATRIX, VIEWMATRIX);
+        cube.draw();
+
+        GL.flush();
+        window.requestAnimationFrame(animate);
+    };
+    animate(0);
+}
+window.addEventListener('load', main);
---
+++ object.js
@@ -0,0 +1,173 @@
+var GL;
+
+class Object {
+  object_vertex = [];
+  OBJECT_VERTEX = GL.createBuffer();
+  object_faces = [];
+  OBJECT_FACES = GL.createBuffer();
+
+  // Local transformation
+  INIT_SCALEMATRIX = glMatrix.mat4.create();
+  INIT_ROTATEMATRIX = glMatrix.mat4.create();
+  INIT_TRANSMATRIX = glMatrix.mat4.create();
+
+  // Transformation
+  SCALEMATRIX = glMatrix.mat4.create();
+  ROTATEMATRICES = [glMatrix.mat4.create()];
+  TRANSMATRIX = glMatrix.mat4.create();
+  origin = [0, 0, 0];
+
+  // Relationship
+  parent = null;
+  child = [];
+
+  // Shaders
+  shader_vertex_source;
+  shader_fragment_source;
+
+  compile_shader = function (source, type, typeString) {
+    var shader = GL.createShader(type);
+    GL.shaderSource(shader, source);
+    GL.compileShader(shader);
+    if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+      alert("ERROR IN " + typeString + " SHADER: " + GL.getShaderInfoLog(shader));
+      return false;
+    }
+    return shader;
+  };
+
+  shader_vertex;
+  shader_fragment;
+  SHADER_PROGRAM;
+  _Pmatrix;
+  _Vmatrix;
+  _Mmatrix;
+  _color;
+  _position;
+
+  constructor(object_vertex, object_faces, shader_vertex_source, shader_fragment_source) {
+    this.object_vertex = object_vertex;
+    this.object_faces = object_faces;
+    this.shader_vertex_source = shader_vertex_source;
+    this.shader_fragment_source = shader_fragment_source;
+
+    this.shader_vertex = this.compile_shader(this.shader_vertex_source, GL.VERTEX_SHADER, "VERTEX");
+    this.shader_fragment = this.compile_shader(this.shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT");
+    this.SHADER_PROGRAM = GL.createProgram();
+
+    GL.attachShader(this.SHADER_PROGRAM, this.shader_vertex);
+    GL.attachShader(this.SHADER_PROGRAM, this.shader_fragment);
+    GL.linkProgram(this.SHADER_PROGRAM);
+
+    this._Pmatrix = GL.getUniformLocation(this.SHADER_PROGRAM, "Pmatrix");
+    this._Vmatrix = GL.getUniformLocation(this.SHADER_PROGRAM, "Vmatrix");
+    this._Mmatrix = GL.getUniformLocation(this.SHADER_PROGRAM, "Mmatrix");
+
+    this._color = GL.getAttribLocation(this.SHADER_PROGRAM, "color");
+    this._position = GL.getAttribLocation(this.SHADER_PROGRAM, "position");
+
+    GL.enableVertexAttribArray(this._color);
+    GL.enableVertexAttribArray(this._position);
+
+    GL.useProgram(this.SHADER_PROGRAM);
+    this.initialize();
+  }
+
+  initialize() {
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.OBJECT_VERTEX);
+    GL.bufferData(GL.ARRAY_BUFFER,
+      new Float32Array(this.object_vertex),
+      GL.STATIC_DRAW);
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.OBJECT_FACES);
+    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER,
+      new Uint16Array(this.object_faces),
+      GL.STATIC_DRAW);
+  }
+
+  translate(tx, ty, tz) {
+    glMatrix.mat4.translate(this.TRANSMATRIX, this.TRANSMATRIX, [tx, ty, tz]);
+    for (let i = 0; i < this.child.length; i++) {
+      this.child[i].translate(tx, ty, tz);
+    }
+  }
+
+  #rotateChildren(ax, ay, az, x, y, z, depth = 1) {
+    while (this.ROTATEMATRICES.length <= depth) {
+      this.ROTATEMATRICES.push(glMatrix.mat4.create());
+    }
+    glMatrix.mat4.translate(this.ROTATEMATRICES[depth], this.ROTATEMATRICES[depth], [x, y, z]);
+    glMatrix.mat4.rotateX(this.ROTATEMATRICES[depth], this.ROTATEMATRICES[depth], ax);
+    glMatrix.mat4.rotateY(this.ROTATEMATRICES[depth], this.ROTATEMATRICES[depth], ay);
+    glMatrix.mat4.rotateZ(this.ROTATEMATRICES[depth], this.ROTATEMATRICES[depth], az);
+    glMatrix.mat4.translate(this.ROTATEMATRICES[depth], this.ROTATEMATRICES[depth], [-x, -y, -z]);
+    for (let i = 0; i < this.child.length; i++) {
+      this.child[i].#rotateChildren(ax, ay, az, x, y, z, depth + 1);
+    }
+  }
+
+  rotate(ax, ay, az) {
+    glMatrix.mat4.translate(this.ROTATEMATRICES[0], this.ROTATEMATRICES[0], this.origin);
+    glMatrix.mat4.rotateX(this.ROTATEMATRICES[0], this.ROTATEMATRICES[0], ax);
+    glMatrix.mat4.rotateY(this.ROTATEMATRICES[0], this.ROTATEMATRICES[0], ay);
+    glMatrix.mat4.rotateZ(this.ROTATEMATRICES[0], this.ROTATEMATRICES[0], az);
+    glMatrix.mat4.translate(this.ROTATEMATRICES[0], this.ROTATEMATRICES[0], [-this.origin[0], -this.origin[1], -this.origin[2]]);
+    for (let i = 0; i < this.child.length; i++) {
+      this.child[i].#rotateChildren(ax, ay, az, this.origin[0], this.origin[1], this.origin[2]);
+    }
+  }
+
+  setUniform4(PROJMATRIX, VIEWMATRIX) {
+    let MOVEMATRIX = glMatrix.mat4.create();
+    glMatrix.mat4.multiply(MOVEMATRIX, this.INIT_SCALEMATRIX, MOVEMATRIX);
+    glMatrix.mat4.multiply(MOVEMATRIX, this.INIT_ROTATEMATRIX, MOVEMATRIX);
+    glMatrix.mat4.multiply(MOVEMATRIX, this.INIT_TRANSMATRIX, MOVEMATRIX);
+
+    glMatrix.mat4.multiply(MOVEMATRIX, this.SCALEMATRIX, MOVEMATRIX);
+    for (let i = 0; i < this.ROTATEMATRICES.length; i++) {
+      glMatrix.mat4.multiply(MOVEMATRIX, this.ROTATEMATRICES[i], MOVEMATRIX);
+    }
+    glMatrix.mat4.multiply(MOVEMATRIX, this.TRANSMATRIX, MOVEMATRIX);
+
+    GL.useProgram(this.SHADER_PROGRAM);
+    GL.uniformMatrix4fv(this._Pmatrix, false, PROJMATRIX);
+    GL.uniformMatrix4fv(this._Vmatrix, false, VIEWMATRIX);
+    GL.uniformMatrix4fv(this._Mmatrix, false, MOVEMATRIX);
+
+    for (let i = 0; i < this.child.length; i++) {
+      this.child[i].setUniform4(PROJMATRIX, VIEWMATRIX);
+    }
+  }
+
+  setPosition(x, y, z) {
+    glMatrix.mat4.fromTranslation(this.INIT_TRANSMATRIX, [x, y, z]);
+  }
+
+  setRotation(ax, ay, az) {
+    glMatrix.mat4.rotateX(this.INIT_ROTATEMATRIX, this.INIT_ROTATEMATRIX, ax);
+    glMatrix.mat4.rotateY(this.INIT_ROTATEMATRIX, this.INIT_ROTATEMATRIX, ay);
+    glMatrix.mat4.rotateZ(this.INIT_ROTATEMATRIX, this.INIT_ROTATEMATRIX, az);
+  }
+
+  setScale(kx, ky, kz) {
+    glMatrix.mat4.scale(this.INIT_SCALEMATRIX, this.INIT_SCALEMATRIX, [kx, ky, kz]);
+  }
+
+  draw() {
+    GL.useProgram(this.SHADER_PROGRAM);
+    GL.bindBuffer(GL.ARRAY_BUFFER, this.OBJECT_VERTEX);
+    GL.vertexAttribPointer(this._position, 3, GL.FLOAT, false, 4 * (3 + 3), 0);
+    GL.vertexAttribPointer(this._color, 3, GL.FLOAT, false, 4 * (3 + 3), 3 * 4);
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, this.OBJECT_FACES);
+    GL.drawElements(GL.TRIANGLES, this.object_faces.length, GL.UNSIGNED_SHORT, 0);
+
+    for (let i = 0; i < this.child.length; i++) {
+      this.child[i].draw();
+    }
+  }
+
+  addChild(object, ox, oy, oz) {
+    object.parent = this;
+    object.origin = [ox, oy, oz];
+    this.child.push(object);
+  }
+}
